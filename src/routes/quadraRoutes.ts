cf31248c9e65245faff18254299e56b27a3b7baa
Victor <victorlima0302@gmail.com>
2024-11-07 17:14:23 -0300

    fix: arrumando as rotas do quadraRoutes para precisar de token

--- src/routes/quadraRoutes.ts
+++ src/routes/quadraRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import * as quadraController from "../controllers/quadraController";
+import { protegerRota } from "../middleware/authMiddleware";
 
 const router = Router();
 
-router.get("/", quadraController.buscarTodasQuadras);
-router.get("/:quadraId/esportes", quadraController.buscarEsportesDaQuadra);
+router.get("/", protegerRota, quadraController.buscarTodasQuadras);
+router.get("/:quadraId/esportes", protegerRota, quadraController.buscarEsportesDaQuadra);
 
 export default router;
