ab0d9b3175f1c6dcf5ef5e18974e6e5e2a2920fd
Victor <victorlima0302@gmail.com>
2024-12-04 19:23:51 -0300

    fix: agora somente admins podem cadastrar usuarios

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { register, login } from '../controllers/authController';
+import { protegerRota } from "../middleware/authMiddleware";
+import { verificarAdmin } from "../middleware/verificarAdmin";
 
 const router = Router();
 
-router.post('/register', register);
+router.post('/register',protegerRota, verificarAdmin, register);
 router.post('/login', login);
 
 export default router;
